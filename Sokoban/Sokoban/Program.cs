6aa1a52817a8cbccd2db47a78304e74f3509ef4e
Unknown <stijn@zmeel.nl>
2018-09-12 15:26:46 +0200

    Created Visual Studio Project
    "hello world"

---
+++ Sokoban/Sokoban/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello world");
+            System.Console.ReadLine();
+        }
+    }
+}
